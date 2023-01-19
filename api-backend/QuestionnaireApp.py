305f11602628eb208f8cd532aabbc0d9bb2aafa8
ntua-el21724 <el21724@mail.ntua.gr>
2023-01-19 15:21:52 +0200

    User verification test

--- api-backend/QuestionnaireApp.py
+++ api-backend/QuestionnaireApp.py
@@ -3,9 +3,11 @@ from flask import Flask, render_template
 # create app and set directory for html code (default is "./templates")
 app = Flask(__name__, template_folder="../frontend")
 
+
 @app.route("/")
 def questionnaire_test():
-    return render_template("main.html")
+    return render_template("index.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
