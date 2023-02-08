27e6fd86d0dd8eeed192a1845e9bb8115616dc14
el19067 <el19067@mail.ntua.gr>
2023-02-09 01:10:25 +0200

    Split APIs - Sites

--- api-backend/QuestionnaireApp.py
+++ api-backend/QuestionnaireApp.py
@@ -20,14 +20,6 @@ client = pymongo.MongoClient("localhost", 27017)
 db = client.queDB
 app.config['JSON_AS_ASCII'] = False
 
-@app.route("/")
-def questionnaire_test():
-    questionnaires = []
-    for questr in db.questionnaire.find():
-        questionnaires.append(
-            (questr['_id'], questr['questionnaireTitle'], questr['questions'][0]['qID']))
-    return render_template("main.html", questionnaires=questionnaires)
-
 
 @app.route("/intelliq_api/questionnaire/<string:slug>", methods=["GET"])
 def get_questionnaire(slug):
@@ -42,23 +34,6 @@ def get_questionnaire(slug):
     if (format=="csv"):
         return 
     return jsonify(questionnaire), 200
-
-
-@app.route("/RadioQuestion/<string:questionnaire_id>/<string:question_id>")
-def setRadioQuestion(questionnaire_id, question_id):
-    qOptions = []
-    qNextIDs = []
-    questionForm = list(db.questionnaire.aggregate([{'$match': {'_id': questionnaire_id}}, {'$unwind': {'path': '$questions'}}, {'$match': {'questions.qID': question_id}}, {'$unset': [
-                        'keywords', 'questionnaireTitle']}, {'$project': {'qID': '$questions.qID', 'qtext': '$questions.qtext', 'required': '$questions.required', 'type': '$questions.type', 'options': '$questions.options'}}]))
-    print(len(questionForm[0].get('options')))  # Gia svisimo
-    if (questionForm[0].get('options')[0].get('optID'))[3] == 'T' or (questionForm[0].get('options')[0].get('optID'))[3] == 'X':
-        return render_template("question_textfield.html", Question=questionForm[0].get('qtext'), questionnaire_id=questionnaire_id, nextQuestion_id=questionForm[0].get('options')[0].get('nextqID'))
-    else:
-        for i in range(len(questionForm[0].get('options'))):
-            qOptions.append(questionForm[0].get('options')[i].get('opttxt'))
-            qNextIDs.append(questionForm[0].get('options')[i].get('nextqID'))
-            # print(qNextIDs[j]) # gia svisimo
-        return render_template("question_radio.html", Question=questionForm[0].get('qtext'), qOptions=qOptions, questionnaire_id=questionnaire_id, qNextIDs=qNextIDs)
 
 
 @app.route("/intelliq_api/question/<string:slug1>/<string:slug2>", methods=["GET"])
@@ -113,6 +88,40 @@ def postreponse(questionnaireID, questionID, session, optionID):
         "qID": questionID,
         "ans": optionID
     })
+
+
+# The aboveees are APIs                  --/\--
+#                                          ||
+#                                          ||
+#                                                              ||
+#                                                              ||
+# The belowees are pages of the website                      --\/--
+
+
+@app.route("/RadioQuestion/<string:questionnaire_id>/<string:question_id>")
+def setRadioQuestion(questionnaire_id, question_id):
+    qOptions = []
+    qNextIDs = []
+    questionForm = list(db.questionnaire.aggregate([{'$match': {'_id': questionnaire_id}}, {'$unwind': {'path': '$questions'}}, {'$match': {'questions.qID': question_id}}, {'$unset': [
+                        'keywords', 'questionnaireTitle']}, {'$project': {'qID': '$questions.qID', 'qtext': '$questions.qtext', 'required': '$questions.required', 'type': '$questions.type', 'options': '$questions.options'}}]))
+    print(len(questionForm[0].get('options')))  # Gia svisimo
+    if (questionForm[0].get('options')[0].get('optID'))[3] == 'T' or (questionForm[0].get('options')[0].get('optID'))[3] == 'X':
+        return render_template("question_textfield.html", Question=questionForm[0].get('qtext'), questionnaire_id=questionnaire_id, nextQuestion_id=questionForm[0].get('options')[0].get('nextqID'))
+    else:
+        for i in range(len(questionForm[0].get('options'))):
+            qOptions.append(questionForm[0].get('options')[i].get('opttxt'))
+            qNextIDs.append(questionForm[0].get('options')[i].get('nextqID'))
+            # print(qNextIDs[j]) # gia svisimo
+        return render_template("question_radio.html", Question=questionForm[0].get('qtext'), qOptions=qOptions, questionnaire_id=questionnaire_id, qNextIDs=qNextIDs)
+
+
+@app.route("/")
+def questionnaire_test():
+    questionnaires = []
+    for questr in db.questionnaire.find():
+        questionnaires.append(
+            (questr['_id'], questr['questionnaireTitle'], questr['questions'][0]['qID']))
+    return render_template("main.html", questionnaires=questionnaires)
 
 
 if __name__ == '__main__':
