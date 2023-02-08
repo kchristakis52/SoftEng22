1502c635e669a10f1527006d4b7234014da43545
ntua-el19952 <el19952@mail.ntua.gr>
2023-02-08 20:03:16 +0200

    Select Questionnaire-->First Question

--- api-backend/QuestionnaireApp.py
+++ api-backend/QuestionnaireApp.py
@@ -12,7 +12,7 @@ db = client.queDB
 def questionnaire_test():
     questionnaires = []
     for questr in db.questionnaire.find():
-        questionnaires.append((questr['_id'], questr['questionnaireTitle']))
+        questionnaires.append((questr['_id'], questr['questionnaireTitle'], questr['questions'][0]['qID']))
     return render_template("main.html", questionnaires=questionnaires)
 
 @app.route("/intelliq_api/questionnaire/<string:slug>", methods=["GET"])
@@ -29,19 +29,18 @@ def get_questionnaire(slug):
 
 
 @app.route("/RadioQuestion/<string:questionnaire_id>/<string:question_id>")
-def setRadioQuestion(questionnaire_id ,question_id): #Έστω πως μπορούμε να βάλουμε ερωτήσεις με έως 6 πιθανές απαντήσεις
+def setRadioQuestion(questionnaire_id ,question_id): 
     qOptions = []
-    for x in range(6):
-        qOptions.append('None')
     questionForm = list(db.questionnaire.aggregate([{'$match':{'_id': questionnaire_id}}, {'$unwind': {'path': '$questions'}}, {'$match': {'questions.qID': question_id}}, {'$unset':['keywords', 'questionnaireTitle']}, {'$project': {'qID': '$questions.qID','qtext': '$questions.qtext','required': '$questions.required','type': '$questions.type','options': '$questions.options'}}]))
     print(len(questionForm[0].get('options')))#Gia svisimo
+    for x in range(len(questionForm[0].get('options'))):
+        qOptions.append('None')
     if (questionForm[0].get('options')[0].get('optID'))[3] == 'T' or (questionForm[0].get('options')[0].get('optID'))[3] == 'X':
         return render_template("question_textfield.html", Question=questionForm[0].get('qtext'))
     else:
-        for i in range(len(questionForm[0].get('options'))):
-            
+        for i in range(len(questionForm[0].get('options'))):  
             qOptions[i] = questionForm[0].get('options')[i].get('opttxt')
-        return render_template("question_radio.html", Question=questionForm[0].get('qtext'), Option1=qOptions[0], Option2=qOptions[1], Option3=qOptions[2], Option4=qOptions[3], Option5=qOptions[4], Option6=qOptions[5])
+        return render_template("question_radio.html", Question=questionForm[0].get('qtext'),qOptions=qOptions )
 
 
 @app.route("/intelliq_api/question/<string:slug1>/<string:slug2>", methods=["GET"])
