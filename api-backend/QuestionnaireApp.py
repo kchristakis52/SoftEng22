834f17663e094f1eca87a19e18dad48d22d35dd2
ntua-el19067 <el19067@mail.ntua.gr>
2023-02-10 23:29:07 +0200

    Show session answers

--- api-backend/QuestionnaireApp.py
+++ api-backend/QuestionnaireApp.py
@@ -6,6 +6,7 @@ import json
 from urllib.request import urlopen
 import pandas as pd
 from pymongo.errors import ConnectionFailure
+import matplotlib
 
 app = Flask(__name__, template_folder="../frontend")
 client = pymongo.MongoClient("localhost", 27017)
@@ -195,12 +196,6 @@ def setRadioQuestion(questionnaire_id, question_id, session_id):
     questionForm = json.loads(string)
     questionForm = [questionForm]
 
-#    questionForm = list(db.questionnaire.aggregate([{'$match': {'_id': questionnaire_id}}, {'$unwind': {'path': '$questions'}}, {'$match': {'questions.qID': question_id}}, {'$unset': [
-#                        'keywords', 'questionnaireTitle']}, {'$project': {'qID': '$questions.qID', 'qtext': '$questions.qtext', 'required': '$questions.required', 'type': '$questions.type', 'options': '$questions.options'}}]))
-    
-#    print(len(questionForm[0].get('options')))  # Gia svisimo
-    print(question_id)
-   
     if (len(questionForm[0].get('options'))) == 1:
         return render_template("question_textfield.html", Question=questionForm[0].get('qtext'), questionnaire_id=questionnaire_id, nextQuestion_id=questionForm[0].get('options')[0].get('nextqID'), optionID=questionForm[0].get('options')[0].get('optID'), question_id=question_id, session_id=session_id)
     else:
@@ -208,7 +203,6 @@ def setRadioQuestion(questionnaire_id, question_id, session_id):
             qOptions.append(questionForm[0].get('options')[i].get('opttxt'))
             qNextIDs.append(questionForm[0].get('options')[i].get('nextqID'))
             qDiffOptions.append(questionForm[0].get('options')[i].get('optID'))
-            # print(qNextIDs[j]) # gia svisimo
         return render_template("question_radio.html", Question=questionForm[0].get('qtext'), qOptions=qOptions, questionnaire_id=questionnaire_id, qNextIDs=qNextIDs, qDiffOptions=qDiffOptions, question_id=question_id, session_id=session_id)
 
 
@@ -231,9 +225,6 @@ def session_answers(slug1, slug2):
                 j['ans']=k['opttxt']
     
     
-    for i in session_dict['answers']:
-        print(i)
-    print(session_dict)
     return render_template("session_answers.html", session_dict=session_dict)
 
 
@@ -255,17 +246,36 @@ def question_answers(slug1, slug2):
             if k['optID'] == j['ans']:
                 j['ans']=k['opttxt']
     
-    
-    for i in session_dict['answers']:
-        print(i)
-    print(session_dict)
     return render_template("session_answers.html", session_dict=session_dict)
+
+
+
+@app.route("/intelliq_api/showsessions/<string:questionnaireID>", methods=["GET"])
+def sessions(questionnaireID):
+    sessions = list(db.responses.aggregate([
+        {
+            '$match': {
+                'questionnaireID': questionnaireID
+            }
+        }, {
+            '$group': {
+                '_id': None, 
+                'uniqueValues': {
+                    '$addToSet': '$session'
+                }
+            }
+        }
+    ]))
+
+    sessions = sessions[0]['uniqueValues']
+    print(sessions)
+    return render_template("question_answers.html", sessions=sessions, questionnaireID=questionnaireID)
+
 
 
 @app.route("/")
 def questionnaire_test():
     session_id = str(uuid.uuid4())[:4]
-    print(session_id)
     questionnaires = []
     for questr in db.questionnaire.find():
         questionnaires.append(
